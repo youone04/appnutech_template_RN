e4063eaf508a1110ada8dc7b0cc7aeccc273599a
youone04 <yudi.gunaone87@gmail.com>
2024-07-11 07:21:59 +0700

    setting alias folder

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
+
